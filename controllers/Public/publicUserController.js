23dc81a381fda80a96c245282689a0751504b777
vojinnk <vojodjukanovic@gmail.com>
2021-02-09 18:09:10 +0100

    testing

--- controllers/Public/publicUserController.js
+++ controllers/Public/publicUserController.js
@@ -1,30 +1,54 @@
+const { Model } = require('sequelize');
 const Advertisment = require('../../models/Advertisment');
 
 
-async function getAddByID(req,res){
+async function getAddByID(req,res,next){
     let id = req.params.id;
     try {        
         let add = await Advertisment.findByPk(id);
+        console.log(add);
         res.status(200).json(add);
     } catch (error) {
-        res.status(400).json(error);
+        if(!error.statusCode){
+            error.statusCode=400;
+        }
+        next(error);
     }
 }
 
-async function getAllAdds(req,res){
-    let id = req.params.id;
+async function getAllAdds(req,res,next){
     try {        
         let adds = await Advertisment.findAll();
         res.status(200).json(adds);
     } catch (error) {
-        res.status(400).json(error);
+        if(!error.statusCode){
+            error.statusCode=400;
+        }
+        next(error);
     }
 }
-async function getAddsByUser(req,res){
-
+async function getAddsByUser(req,res,next){
+    let username=req.params.username;
+    try {
+        let adds = await Advertisment.findAll({
+            include:{
+                model: User,
+                where:{
+                    name: username
+                }
+                
+            }
+        });
+        res.status(200).json(adds);
+    } catch (error) {
+        if(!error.statusCode){
+            error.statusCode=400;
+        }
+        next(error);
+    }
 }
 
-async function getFilteredAdds(req,res){
+async function getFilteredAdds(req,res,next){
 
 }
 async function getAddComments(req,res){
