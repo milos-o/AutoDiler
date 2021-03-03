237a56546df425cbe9c1a268b568ed6f200eb4fa
vojinnk <vojodjukanovic@gmail.com>
2021-03-03 15:30:42 +0100

    msg

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('autodiler', process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
+const sequelize = new Sequelize(process.env.RDS_DBNAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
   dialect: 'mysql',
   host: process.env.RDS_HOSTNAME,
   port: process.env.RDS_PORT
